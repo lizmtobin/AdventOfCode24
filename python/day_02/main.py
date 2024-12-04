ab402d967e4027c88cfe2cc657cc038daf14c554
liz tobin <lizmtobin@gmail.com>
2024-12-04 15:32:40 +0000

    add d2, part two

--- python/day_02/main.py
+++ python/day_02/main.py
@@ -17,7 +17,6 @@ def is_safe(numbers):
       return False
   # If all checks pass, return True
   return True
-    
 
 def part1():
   reports = []
@@ -36,8 +35,25 @@ def part1():
     
 def part2():
   reports = []
+  safe_level_reports = 0
+  
   with open('python/day_02/puzzle_input.txt') as f:
     reports = f.readlines()
+    
+  for report in reports:
+    numbers = list(map(int,report.split()))
+    
+    if is_safe(numbers):
+      safe_level_reports += 1
+    else:
+      # if not currently safe -  check if it can be made safe by removing one
+      for i in range(len(numbers)):
+        # remove one number from the list and check if the new list is safe
+        if is_safe(numbers[:i] + numbers[i+1:]):
+          safe_level_reports += 1
+          break
+      
+  print(safe_level_reports)
     
 if __name__ == '__main__':
   part1()
@@ -64,3 +80,12 @@ if __name__ == '__main__':
 
 # Output:
 #   Count the number of safe reports
+
+# Part 2:
+  
+# the problem reports are high but ther is a dampner
+# The dampner means a single bad level in a report is tolerable
+
+# Approach:
+  # same as above but keep count of bad outcomes instead of straigh to false
+  # if bad outcomes are more than 1, then the report is unsafe
---
+++ python/day_02/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+        numbers.remove(b)
